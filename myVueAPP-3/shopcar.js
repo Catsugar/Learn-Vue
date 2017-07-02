8877f3e420c1b528ee1c4f700a5c902a6fe4d6a4
catsugar <773817248@qq.com>
2017-07-02 15:35:05 +0800

    111

--- myVueAPP-3/shopcar.js
+++ myVueAPP-3/shopcar.js
@@ -41,24 +41,36 @@ var vm = new Vue({
 	selectPro: function(product){
 	    if(typeof product.checked=='undefined'){
 		  //Vue.set(product,"checked",true);//全局注册
-		  this.$set(product,"checked",true);局部注册
+		  this.$set(product,"checked",true);//局部注册
 		}else{
 		   product.checked=!product.checked;	
+		}
+	},
+    selectShop: function(shop){
+	    if(typeof shop.checked=='undefined'){
+		  this.$set(shop,"checked",true);//局部注册
+		  var _this=this;
+		  this.product.forEach(function(pro,index){
+		    if(typeof pro.checked=='undefined'){
+		      _this.$set(pro,"checked",_this.checked);//局部注册
+		    }else{
+		      pro.checked=_this.checked;	
+		    }
+		  })
+		}else{
+		   shop.checked=!shop.checked;	
 		}
 	},
 	checkAll: function(){
 		this.checkAllFlag=!this.checkAllFlag;
 		var _this=this;
-		if(this.checkAllFlag){
-			this.shopList.forEach(function(product,index){
-			  if(typeof product.checked=='undefined'){
-		         _this.$set(product,"checked",true);局部注册
-		      }else{
-		          product.checked=true;	
-		      }
-			
-			})
-		}
+	    this.shopList.forEach(function(shop,index){
+		  if(typeof shop.checked=='undefined'){
+		     _this.$set(shop,"checked",_this.checkAllFlag);//局部注册
+		  }else{
+		     shop.checked=_this.checkAllFlag;	
+		  }
+		})
 	}
   }
 });
